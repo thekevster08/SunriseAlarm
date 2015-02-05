ca686180e0d041dac5329bb94b2274c12cba11bd
Kevin Collins <kevin.collins@outlook.com>
2015-02-04 20:24:58 -0500

    crude stuff set

--- BluetoothTest2/app/src/main/java/com/example/kevin/bluetoothtest2/MainActivity.java
+++ BluetoothTest2/app/src/main/java/com/example/kevin/bluetoothtest2/MainActivity.java
@@ -1,32 +1,98 @@
 package com.example.kevin.bluetoothtest2;
 
+import android.app.Activity;
+import android.app.DatePickerDialog;
+import android.app.Dialog;
+import android.app.TimePickerDialog;
+import android.app.DialogFragment;
 import android.bluetooth.BluetoothAdapter;
 import android.bluetooth.BluetoothDevice;
 import android.bluetooth.BluetoothServerSocket;
 import android.bluetooth.BluetoothSocket;
+import android.os.Handler;
+import android.support.v4.app.FragmentActivity;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.text.format.DateFormat;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.DatePicker;
+import android.widget.TimePicker;
 import android.widget.Toast;
 
 import java.io.IOException;
+import java.io.InputStream;
 import java.io.OutputStream;
+import java.text.Format;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
 import java.util.UUID;
 
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+import android.widget.ToggleButton;
+import android.app.Fragment;
 
-public class MainActivity extends ActionBarActivity {
+public class MainActivity extends FragmentActivity implements TimePickerDialog.OnTimeSetListener{
 
     BluetoothAdapter mBluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
     android.bluetooth.BluetoothSocket btSocket;
 
     private static final String TAG = "kev";
     private OutputStream outStream = null;
+    private InputStream inStream = null;
+    Handler  handler = new Handler();
+    byte delimiter = 10;
+    boolean stopWorker = false;
+    int readBufferPosition = 0;
+    byte[] readBuffer = new byte[1024];
+
+    TextView receivedMessage;
+    Button setAlarmButton, setTimeButton;
+    TimePicker timePicker;
+    private int hour;
+    private int minute;
+    private int second;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        receivedMessage = (TextView) findViewById(R.id.receivedMessage);
+        setAlarmButton = (Button) findViewById(R.id.setAlarmButton);
+        setTimeButton = (Button) findViewById(R.id.setTimeButton);
+
+        setAlarmButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Calendar c = Calendar.getInstance();
+
+                new TimePickerDialog(MainActivity.this, new TimePickerDialog.OnTimeSetListener() {
+
+                    @Override
+                    public void onTimeSet(TimePicker view, int hourOfDay, int minute) {
+                        receivedMessage.setText("Time: " + hourOfDay + " : " + minute);
+                    }
+                },
+                        c.get(Calendar.HOUR_OF_DAY),
+                        c.get(Calendar.MINUTE),
+                        true).show();
+            }
+        });
+
+//https://github.com/Bootez/TimeDialog/blob/master/src/com/example/timedialog/MainActivity.java
+
+
+
+        setTimeButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                writeData("2");
+            }
+        });
 
         CheckBt();
         Connect();
@@ -92,7 +158,7 @@ public class MainActivity extends ActionBarActivity {
             Log.d("", "Socket creation failed");
         }
 
-      //  beginListenForData();
+        beginListenForData();
                /* this is a method used to read what the Arduino says for example when you write Serial.print("Hello world.") in your Arduino code */
     }
 
@@ -114,6 +180,85 @@ public class MainActivity extends ActionBarActivity {
         }
     }
 
+
+    public void beginListenForData()   {
+        try {
+            inStream = btSocket.getInputStream();
+        } catch (IOException e) {
+        }
+
+        Thread workerThread = new Thread(new Runnable()
+        {
+            public void run()
+            {
+                while(!Thread.currentThread().isInterrupted() && !stopWorker)
+                {
+                    try
+                    {
+                        int bytesAvailable = inStream.available();
+                        if(bytesAvailable > 0)
+                        {
+                            byte[] packetBytes = new byte[bytesAvailable];
+                            inStream.read(packetBytes);
+                            for(int i=0;i<bytesAvailable;i++)
+                            {
+                                byte b = packetBytes[i];
+                                if(b == delimiter)
+                                {
+                                    byte[] encodedBytes = new byte[readBufferPosition];
+                                    System.arraycopy(readBuffer, 0, encodedBytes, 0, encodedBytes.length);
+                                    final String data = new String(encodedBytes, "US-ASCII");
+                                    readBufferPosition = 0;
+                                    handler.post(new Runnable()
+                                    {
+                                        public void run()
+                                        {
+
+                                            if(receivedMessage.getText().toString().equals("..")) {
+                                                receivedMessage.setText(data);
+                                            } else {
+                                                receivedMessage.append("\n" + data);
+                                            }
+
+	                                        	/* You also can use Result.setText(data); it won't display multilines
+	                                        	*/
+
+                                        }
+                                    });
+                                }
+                                else
+                                {
+                                    readBuffer[readBufferPosition++] = b;
+                                }
+                            }
+                        }
+                    }
+                    catch (IOException ex)
+                    {
+                        stopWorker = true;
+                    }
+                }
+            }
+        });
+
+        workerThread.start();
+    }
+
+    @Override
+    public void onTimeSet(TimePicker view, int hour, int minute)
+    {
+        this.hour = hour;
+        this.minute = minute;
+
+        Calendar calendar = Calendar.getInstance();
+        calendar.set(Calendar.HOUR, hour);
+        calendar.set(Calendar.MINUTE, minute);
+
+        Format formatter = new SimpleDateFormat("HH:mm:ss");
+        String s = formatter.format(calendar.getTime());
+
+        receivedMessage.setText(s);
+    }
 
 
 }
